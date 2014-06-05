edb94f551a21a0e31de7d45c26ffc30502b744e8
Marius Küng <apps@mariuskueng.ch>
2014-06-05 13:56:55 +0200

    Added jquery, remote data source, cleanups

--- main.js
+++ main.js
@@ -3,58 +3,69 @@ function Concert(d, a, v) {
     this.date = d;
     this.artist = a;
     this.venue = v;
+    this.city;
 }
 
-// get all contents as node objects
-var concertData = document.querySelectorAll("#content p");
-// concertData = concertData.pop(concertData.lastIndexOf);
-var concertDataText = "";
-var re = new RegExp("<br>", 'g');
+function loadConcertData(data) {
+    var concertData = data.find("#content p");
 
-// convert content to string
-for (var i = 0; i < concertData.length - 3; i++) {
-    concertDataText += concertData[i].innerHTML;
-};
+    // get all contents as node objects
 
-// remove html line breaks
-concertDataText = concertDataText.replace(re, '')
+    // concertData = concertData.pop(concertData.lastIndexOf);
+    var concertDataText = "";
+    var re = new RegExp("<br>", 'g');
 
-// separate each line into array item
-concertData = concertDataText.split("\n");
+    // convert content to string
+    for (var i = 0; i < concertData.length - 3; i++) {
+        concertDataText += concertData[i].innerHTML;
+    };
 
-var concerts = [];
-var substringIndex = 0;
+    // remove html line breaks
+    concertDataText = concertDataText.replace(re, '')
 
-for (var i = 0; i < concertData.length; i++) {
-    var date;
-    var dateRange = concertData[i].substring(0, 11);
-    var artist;
-    var venue;
+    // separate each line into array item
+    concertData = concertDataText.split("\n");
 
-    if (dateRange.indexOf("–") > -1) { // is festival
-        substringIndex = 11;
-        date = concertData[i].substring(0, substringIndex);
-        venue = concertData[i].split("mit");
-        venue = venue[0].substring(substringIndex, venue[0].length)
-        artist = concertData[i].substring(date.length + venue.length, concertData[i].length).replace("mit ", "");
-    } else { // is single concert
-        substringIndex = 5;
-        date = concertData[i].substring(0, substringIndex);
-        artist = concertData[i].substring(substringIndex + 1, concertData[i].length).split(",")[0];
-        venue = concertData[i].substring(substringIndex + 1, concertData[i].length).split(",")[1].split("\n")[0];
-    }
-    concerts.push(
-        new Concert(
-            date,
-            artist.trim(),
-            venue.trim()
-    ));
-};
+    var concerts = [];
+    var substringIndex = 0;
 
-var concertNode = document.getElementById("concerts");
+    for (var i = 0; i < concertData.length; i++) {
+        var date;
+        var dateRange = concertData[i].substring(0, 11);
+        var artist;
+        var venue;
 
-for (var i = 0; i < concerts.length; i++) {
-    var e = document.createElement("p")
-    e.innerHTML = concerts[i].date + ": " + concerts[i].artist + ", " + concerts[i].venue;
-    concertNode.appendChild(e);
-};
+        if (dateRange.indexOf("–") > -1) { // is festival
+            substringIndex = 11;
+            date = concertData[i].substring(0, substringIndex);
+            venue = concertData[i].split("mit");
+            venue = venue[0].substring(substringIndex, venue[0].length)
+            artist = concertData[i].substring(date.length + venue.length, concertData[i].length).replace("mit ", "");
+        } else { // is single concert
+            substringIndex = 5;
+            date = concertData[i].substring(0, substringIndex);
+            artist = concertData[i].substring(substringIndex + 1, concertData[i].length).split(",")[0];
+            venue = concertData[i].substring(substringIndex + 1, concertData[i].length).split(",")[1].split("\n")[0];
+        }
+        concerts.push(
+            new Concert(
+                date,
+                artist.trim(),
+                venue.trim()
+        ));
+    };
+
+    var concertNode = $("#concerts");
+
+    for (var i = 0; i < concerts.length; i++) {
+        concertNode.append("<p>" + concerts[i].date + ": " + concerts[i].artist + ", " + concerts[i].venue + "</p>");
+    };
+}
+
+$(document).ready(function(){
+
+    // $.get("http://mainstream.radiox.ch/konzerte", function(data){
+    //     loadConcertData($(data));
+    // });
+
+});
