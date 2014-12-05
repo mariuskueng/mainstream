11a5502d195d5e9e7c0d14b0a4003250f3194678
Marius Küng <apps@mariuskueng.ch>
2014-12-05 11:38:03 +0100

    Added moment.js

--- client/lib/concerts.js
+++ client/lib/concerts.js
@@ -4,4 +4,8 @@ Template.concerts.helpers({
     concerts: function() {
         return Concerts.find({}, {sort: [['date', 'asc']]});
     }
-})
+});
+
+Handlebars.registerHelper("prettifyDate", function(timestamp) {
+    return moment(new Date(timestamp)).format("DD.M.");
+});
